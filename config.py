2e52f57de86faef3711127639da647b7a2f2ef22
Vitlaii Tsurka <Fakerovi4i@gmail.com>
2026-04-21 12:35:35 +0300

    Домашний ноут

--- config.py
+++ config.py
@@ -1,15 +1,18 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 
-
-if not find_dotenv():
-    exit("Переменные окружения не загружены, так как отсутствует файл .env")
-else:
-    load_dotenv()
+# Сделано для развертывания
+dotenv_path = find_dotenv()
+if dotenv_path:
+    load_dotenv(dotenv_path)
+# Локально
+# if not find_dotenv():
+#     exit("Переменные окружения не загружены, так как отсутствует файл .env")
+# else:
+#     load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
 API_KEY = os.getenv('API_KEY')
 API_MODEL = os.getenv('API_MODEL')
-
 
 API_TIMEOUT = 30
